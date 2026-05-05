83c93c8a544652f513f03a3517e9c48f6f0aea66
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2026-05-04 21:21:56 -0300

    Addi ValidationConfig para puxar as mensagens traduizadas

---
+++ src/main/java/com/fiap/gastrolinkapi/infra/config/ValidationConfig.java
@@ -0,0 +1,26 @@
+package com.fiap.gastrolinkapi.infra.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:ValidationMessages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        factory.setValidationMessageSource(messageSource);
+        return factory;
+    }
+}
